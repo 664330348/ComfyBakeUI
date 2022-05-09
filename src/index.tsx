e5c346967c4a06d4c1f8f564b6484cbc42bcafc5
zhenying <a664330348@gmail.com>
2022-05-09 14:57:21 -0400

    comfybake ui initial

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
+import HomePage from './Home';
+
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <HomePage />
     </Provider>
   </React.StrictMode>
 );
